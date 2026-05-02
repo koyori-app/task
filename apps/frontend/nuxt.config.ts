7026fe860abed79b81999ad86866eefc73f47407
default <sousuke20xx@gmail.com>
2026-05-02 21:46:07 +0000

    chore(front): install tailwindcss

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,8 +1,16 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxtjs/seo", "@pinia/nuxt", "@nuxtjs/google-fonts"],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   nitro: {
     preset: 'bun'
   }
