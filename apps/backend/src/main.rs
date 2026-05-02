9edf19154e0eb776833c6e9ae50e59dd7639c2aa
yupix <yupi0982@outlook.jp>
2026-05-02 21:29:43 +0000

    feat(backend): 簡易的なサーバを構築

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use utoipa_scalar::{Scalar, Servable};
+
+
+#[tokio::main]
+async fn main() {
+    let (mut router, openapi) =
+        utoipa_axum::router::OpenApiRouter::new().route("/", get(|| async { "Hello, world!" })).split_for_parts();
+
+    router = router.merge(Scalar::with_url("/scalar", openapi.clone()));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+
+    println!("Listening on http://0.0.0.0:3000");
+    axum::serve(listener, router).await.unwrap();
 }
