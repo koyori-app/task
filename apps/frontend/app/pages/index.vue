c511caa3c05e4cd758caba30f5d86d3b15e9f527
default <sousuke20xx@gmail.com>
2026-05-03 11:06:37 +0000

    chore(front): 初期画面無効化

---
+++ apps/frontend/app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 class="text-4xl font-bold mb-4">Welcome to the Nuxt 3 Starter Template!</h1>
+        <p class="text-lg text-gray-600 mb-8">This is the home page of your Nuxt 3 application.</p>
+        <nuxt-link to="/about" class="text-blue-500 hover:underline">Go to About Page</nuxt-link>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
