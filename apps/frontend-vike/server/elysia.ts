267e456a252ff7186f275deb4e0e2f53a5d3acba
default <sousuke20xx@gmail.com>
2026-05-12 17:26:57 +0000

    feat(front-vike): env検証

--- apps/frontend-vike/server/elysia.ts
+++ apps/frontend-vike/server/elysia.ts
@@ -1,11 +1,12 @@
 import { createTodoHandler } from "./create-todo-handler";
+import { settingInjector } from "./middlewares/setting-injector";
 import vike from '@vikejs/elysia';
 import { Elysia } from 'elysia';
 
 function getApp() {
   const app = new Elysia();
 
-  vike(app, [createTodoHandler]);
+  vike(app, [settingInjector, createTodoHandler]);
 
   return app;
 }
---
+++ apps/frontend-vike/server/settings/env.ts
@@ -0,0 +1,10 @@
+import arkenv from "arkenv";
+
+export const appEnvSettings = arkenv({
+  // Main System
+  APP_URL: "string.url",
+
+  // Umami Analytics
+  "UMAMI_HOST?": "string.url",
+  "UMAMI_WEBSITE_ID?": "string.uuid.v4",
+});
