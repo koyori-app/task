07914a6134a1061af72bb0a5a81239af335f9b34
yupix <yupi0982@outlook.jp>
2026-05-12 13:38:53 +0000

    fix(backend): 今後レスポンスでエンティティを指定した際にpassword_hashが表に出ないように修正

--- apps/backend/src/entities/users.rs
+++ apps/backend/src/entities/users.rs
@@ -1,24 +1,25 @@
 use sea_orm::entity::prelude::*;
+use serde::Serialize;
 use utoipa::ToSchema; // Scalar/OpenAPI用
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, ToSchema)]
+#[derive(Clone, Serialize, Debug, PartialEq, DeriveEntityModel, Eq, ToSchema)]
 #[sea_orm(table_name = "users")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)] // auto_incrementを無効にする
-    #[schema(value_type = String, format="uuid")]  // OpenAPIでUUIDとして扱うための属性
+    #[schema(value_type = String, format="uuid")] // OpenAPIでUUIDとして扱うための属性
     pub id: Uuid,
     #[schema(value_type = String, format="username")]
     pub username: String,
     #[sea_orm(nullable)]
     #[schema(nullable)]
-    pub bio: String,
+    pub bio: Option<String>,
     #[sea_orm(nullable)]
     #[schema(nullable)]
     pub avatar_url: Option<String>,
     #[schema(value_type = String, format="email")]
     pub email: String,
     #[sea_orm(nullable)]
-    #[schema(nullable)]
+    #[schema(nullable, ignore)]
     pub password_hash: Option<String>,
 }
 
